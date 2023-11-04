d0725dedbb46825ebea50dbc591284532f15d5d6
xdavide9 <biancodavidetech@gmail.com>
2023-11-04 16:28:43 +0100

    Implement JwtAuthenticationFilter
    Created JwtAuthenticationFilter.java and appropriate tests for it in JwtAuthenticationFilterTest.java

---
+++ src/main/java/com/xdavide9/sso/security/JwtAuthenticationFilter.java
@@ -0,0 +1,100 @@
+package com.xdavide9.sso.security;
+
+import com.xdavide9.sso.user.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * This class is the heart of jwt security configuration. The filter is executed only
+ * once per request and uses the Authorization header (which is supposed to be sent
+ * with every request) to retrieve the jwt token. It then proceeds to validate the token and register
+ * an authentication for the current request only (stateless). If the token is not present an unauthorised code
+ * is sent straight back to the client otherwise the FilterChain proceeds with other filters.
+ * @author xdavide9
+ * @since 0.0.1-SNAPSHOT
+ * @see com.xdavide9.sso.config.SecurityConfig
+ */
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    /**
+     * Service used to manage operations with jwt tokens
+     * @since 0.0.1-SNAPSHOT
+     */
+    private final JwtService jwtService;
+    /**
+     * It is defined in {@link com.xdavide9.sso.config.SecurityConfig}
+     * @since 0.0.1-SNAPSHOT
+     */
+    private final UserDetailsService userDetailsService;
+
+    /**
+     * constructor
+     * @param jwtService jwtService
+     * @param userDetailsService userDetailsService
+     */
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    /**
+     * The actual filter implementation that processes the filters
+     * @param request client request
+     * @param response server response
+     * @param filterChain filterChain
+     * @throws ServletException exception
+     * @throws IOException exception
+     */
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("Missing Jwt token.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String jwt = authHeader.substring(7);
+        final String username = jwtService.extractUsername(jwt); // can also be the email
+        if (username != null && securityContext().getAuthentication() == null) {
+            User user = (User) userDetailsService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(jwt, user)) {
+                // if token is not valid appropriate exceptions are thrown
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                user,
+                                null,
+                                user.getAuthorities()
+                        );
+                authToken.setDetails(
+                        new WebAuthenticationDetails(request)
+                );
+                securityContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    protected SecurityContext securityContext() {
+        return SecurityContextHolder.getContext();
+    }
+}
---
+++ src/test/java/com/xdavide9/sso/security/JwtAuthenticationFilterTest.java
@@ -0,0 +1,107 @@
+package com.xdavide9.sso.security;
+
+import com.xdavide9.sso.user.User;
+import jakarta.servlet.FilterChain;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import static java.lang.String.format;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+// unit test for JwtAuthenticationFilter
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationFilterTest {
+
+    @InjectMocks
+    @Spy
+    private JwtAuthenticationFilter underTest;
+    @Mock
+    private JwtService jwtService;
+    @Mock
+    private UserDetailsService userDetailsService;
+    private MockHttpServletRequest request;
+
+    private MockHttpServletResponse response;
+    @Mock
+    private FilterChain filterChain;
+    @Mock
+    private SecurityContext securityContext;
+
+    @BeforeEach
+    void setUp() {
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    void itShouldDoFilterInternalCorrectlyWithValidToken() throws Exception {
+        // given
+        String username = "xdavide9";
+        User user = new User();
+        user.setUsername(username);
+        given(userDetailsService.loadUserByUsername(username)).willReturn(user);
+        String token = "validToken";
+        request.addHeader("Authorization", format("Bearer %s", token));
+        given(jwtService.extractUsername(token)).willReturn(username);
+        given(jwtService.isTokenValid(token, user)).willReturn(true);
+        ArgumentCaptor<Authentication> captor = ArgumentCaptor.forClass(Authentication.class);
+        given(underTest.securityContext()).willReturn(securityContext);
+        // when
+        underTest.doFilterInternal(request, response, filterChain);
+        // then
+        verify(securityContext).setAuthentication(captor.capture());
+        Authentication capturedAuthentication = captor.getValue();
+        assertThat(capturedAuthentication).isInstanceOf(UsernamePasswordAuthenticationToken.class);
+        assertThat(capturedAuthentication.getPrincipal()).isEqualTo(user);
+        assertThat(capturedAuthentication.getCredentials()).isNull();
+        assertThat(capturedAuthentication.getAuthorities()).isEqualTo(user.getAuthorities());
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    void itShouldRespondWithUnauthorisedIfTheTokenIsMissing() throws Exception {
+        // given
+        request.addHeader("Authorization", "invalid token");
+        // when
+        underTest.doFilterInternal(request, response, filterChain);
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(response.getContentAsString()).isEqualTo("Missing Jwt token.");
+    }
+
+    @Test
+    void itShouldDoNothingIfUserIsAlreadyAuthenticated() throws Exception {
+        // given
+        String username = "xdavide9";
+        User user = new User();
+        user.setUsername(username);
+        String token = "validToken";
+        request.addHeader("Authorization", format("Bearer %s", token));
+        given(jwtService.extractUsername(token)).willReturn(username);
+        given(underTest.securityContext()).willReturn(securityContext);
+        given(securityContext.getAuthentication())
+                .willReturn(new TestingAuthenticationToken(user, ""));  // not null
+        // when
+        underTest.doFilterInternal(request, response, filterChain);
+        // then
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+}
