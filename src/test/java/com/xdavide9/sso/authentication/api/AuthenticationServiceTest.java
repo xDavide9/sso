a23b8e626a4dbbc7783a466131959c946dde7d85
xdavide9 <biancodavidetech@gmail.com>
2023-11-12 12:28:10 +0100

    Finish tests in AuthenticationServiceTest
    Created all test cases for signup method in AuthenticationServiceTest.java; configured pom.xml to suppress some warnings

--- src/test/java/com/xdavide9/sso/authentication/api/AuthenticationServiceTest.java
+++ src/test/java/com/xdavide9/sso/authentication/api/AuthenticationServiceTest.java
@@ -4,6 +4,9 @@ package com.xdavide9.sso.authentication.api;
 
 import com.xdavide9.sso.authentication.AuthenticationResponse;
 import com.xdavide9.sso.authentication.SignupRequest;
+import com.xdavide9.sso.exception.authentication.api.EmailTakenException;
+import com.xdavide9.sso.exception.authentication.api.PasswordTooShortException;
+import com.xdavide9.sso.exception.authentication.api.UsernameTakenException;
 import com.xdavide9.sso.jwt.JwtService;
 import com.xdavide9.sso.user.User;
 import com.xdavide9.sso.user.UserRepository;
@@ -18,9 +21,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Objects;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,5 +72,61 @@ class AuthenticationServiceTest {
         verifyNoMoreInteractions(jwtService);
         verifyNoMoreInteractions(validator);
         verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    void itShouldNotSignupUsernameTakenAndThrow() {
+        // given
+        String username = "xdavide9";
+        String password = "password"; // >= 8 characters
+        String email = "email@sso.com";
+        given(repository.existsByUsername(username)).willReturn(true);
+        SignupRequest request = new SignupRequest(username, email, password);
+        // when & then
+        assertThatThrownBy(() -> underTest.signup(request))
+                .isInstanceOf(UsernameTakenException.class)
+                .hasMessageContaining(format("Username [%s] is already taken", username));
+        verifyNoMoreInteractions(repository);
+        verifyNoInteractions(jwtService);
+        verifyNoInteractions(validator);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void itShouldNotSignupEmailTakenAndThrow() {
+        // given
+        String username = "xdavide9";
+        String password = "password"; // >= 8 characters
+        String email = "email@sso.com";
+        given(repository.existsByUsername(username)).willReturn(false);
+        given(repository.existsByEmail(email)).willReturn(true);
+        SignupRequest request = new SignupRequest(username, email, password);
+        // when & then
+        assertThatThrownBy(() -> underTest.signup(request))
+                .isInstanceOf(EmailTakenException.class)
+                .hasMessageContaining(format("Email [%s] is already taken", email));
+        verifyNoMoreInteractions(repository);
+        verifyNoInteractions(jwtService);
+        verifyNoInteractions(validator);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void itShouldNotSignupPasswordTooShortAndThrow() {
+        // given
+        String username = "xdavide9";
+        String password = "short"; // < 8 characters
+        String email = "email@sso.com";
+        given(repository.existsByUsername(username)).willReturn(false);
+        given(repository.existsByEmail(email)).willReturn(false);
+        SignupRequest request = new SignupRequest(username, email, password);
+        // when & then
+        assertThatThrownBy(() -> underTest.signup(request))
+                .isInstanceOf(PasswordTooShortException.class)
+                .hasMessageContaining("Password must be at least 8 characters long");
+        verifyNoMoreInteractions(repository);
+        verifyNoInteractions(jwtService);
+        verifyNoInteractions(validator);
+        verifyNoInteractions(passwordEncoder);
     }
 }
