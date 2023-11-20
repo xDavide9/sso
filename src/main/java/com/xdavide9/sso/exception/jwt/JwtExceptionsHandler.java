0fb112d3b8e7efdb4a8d158924c547442a1fe467
xdavide9 <biancodavidetech@gmail.com>
2023-11-20 18:36:52 +0100

    Handle exceptions in JwtExceptionsHandler
    Handled MissingTokenException and ExpiredJwtException in JwtExceptionsHandler.java by returning appropriate responses to clients.

--- src/main/java/com/xdavide9/sso/exception/jwt/JwtExceptionsHandler.java
+++ src/main/java/com/xdavide9/sso/exception/jwt/JwtExceptionsHandler.java
@@ -2,9 +2,14 @@ package com.xdavide9.sso.exception.jwt;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 /**
  * This class is the Exception Handler for exception related to jwt tokens.
@@ -26,12 +31,35 @@ public class JwtExceptionsHandler {
     }
 
     /**
-     * Exception handler
+     * Exception handler for {@link ExpiredJwtException}.
+     * Returns a json response with details about what went wrong to clients.
+     * The token provided with the request is expired.
+     * @param e exception handled
      * @since 0.0.1-SNAPSHOT
      * @return redirect
      */
     @ExceptionHandler(value = ExpiredJwtException.class)
-    public RedirectView redirectToLoginPage() {
-        return new RedirectView("/login?error=expired");
+    public ResponseEntity<?> handleExpiredJwtException(ExpiredJwtException e) {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("error", "Expired Jwt Token");
+        responseBody.put("message", e.getMessage());
+        responseBody.put("status", UNAUTHORIZED);
+        return new ResponseEntity<>(responseBody, UNAUTHORIZED);
+    }
+
+    /**
+     * Exception handler for {@link MissingTokenException}.
+     * Returns a json response with details about want when wrong to clients.
+     * Every request to a secured resource must include a jwtToken that is later processed.
+     * @since 0.0.1-SNAPSHOT
+     * @return redirect
+     */
+    @ExceptionHandler(value = MissingTokenException.class)
+    public ResponseEntity<?> handleMissingTokenException(MissingTokenException e) {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("error", "Missing Jwt Token");
+        responseBody.put("message", e.getMessage());
+        responseBody.put("status", UNAUTHORIZED);
+        return new ResponseEntity<>(responseBody, UNAUTHORIZED);
     }
 }
