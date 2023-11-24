8103900d9548ae1c448a7c3c5753a1d602563ba5
xdavide9 <biancodavidetech@gmail.com>
2023-11-24 16:02:00 +0100

    Improve JsonParserService.java
    Used DI in JsonParserService.java and test it

--- src/main/java/com/xdavide9/sso/util/JsonParserService.java
+++ src/main/java/com/xdavide9/sso/util/JsonParserService.java
@@ -2,15 +2,27 @@ package com.xdavide9.sso.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Utility class that uses Jackson's {@link ObjectMapper} to convert java objects to json and vice-versa.
  * @since 0.0.1-SNAPSHOT
  * @author xdavide9
  */
-public class JsonParser {
+@Service
+public class JsonParserService {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper;
+
+    @Autowired
+    public JsonParserService(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(JsonParserService.class);
 
     /**
      * converts java objects to json. In case of failure an error message in capital letters is thrown.
@@ -18,12 +30,13 @@ public class JsonParser {
      * @return json of the object passed
      * @since 0.0.1-SNAPSHOT
      */
-    public static String json(Object o) {
+    public String json(Object o) {
         try {
             String json = mapper.writeValueAsString(o);
-            System.out.println("JACKSON PARSING OBJECT " + o + " TO JSON " + json);
-            return mapper.writeValueAsString(o);
+            log.info("JACKSON PARSING OBJECT " + o + " TO JSON " + json);
+            return json;
         } catch (JsonProcessingException e) {
+            log.error("JACKSON CANNOT CONVERT JAVA OBJECT " + o + " TO JSON");
             throw new RuntimeException("JACKSON CANNOT CONVERT JAVA OBJECT " + o + " TO JSON");
         }
     }
@@ -36,11 +49,13 @@ public class JsonParser {
      * @return the object converted from json
      * @since 0.0.1-SNAPSHOT
      */
-    public static<T> T java(String json, Class<T> type) {
+    public<T> T java(String json, Class<T> type) {
         try {
+            log.info("PARSING JSON " + json + " TO A JAVA OBJECT OF TYPE " + type);
             return mapper.readValue(json, type);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("JACKSON CANNOT CONVERT JSON " + json + " TO JAVA OBJECT");
+            log.error("JACKSON CANNOT CONVERT JSON " + json + " TO JAVA OBJECT OF TYPE " + type);
+            throw new RuntimeException("JACKSON CANNOT CONVERT JSON " + json + " TO JAVA OBJECT OF TYPE " + type);
         }
     }
 }
---
+++ src/test/java/com/xdavide9/sso/util/JsonParserServiceTest.java
@@ -0,0 +1,71 @@
+package com.xdavide9.sso.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xdavide9.sso.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class JsonParserServiceTest {
+
+    @InjectMocks
+    private JsonParserService underTest;
+
+    @Mock
+    private ObjectMapper mapper;
+
+    @Test
+    void itShouldConvertJavaToJson() throws Exception {
+        // given
+        Object o = new Object();
+        given(mapper.writeValueAsString(o)).willReturn("\"key\":\"value\"");
+        // when
+        String json = underTest.json(o);
+        // then
+        assertThat(json).isEqualTo("\"key\":\"value\"");
+    }
+
+    @Test
+    void itShouldNotConvertJavaToJsonAndThrow() throws Exception {
+        // given
+        Object o = new Object();
+        given(mapper.writeValueAsString(o)).willThrow(JsonProcessingException.class);
+        // when & then
+        assertThatThrownBy(() -> underTest.json(o))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("JACKSON CANNOT CONVERT JAVA OBJECT " + o + " TO JSON");
+    }
+
+    @Test
+    void itShouldCovertJsonToJava() throws Exception {
+        // given
+        Object o = new Object();
+        String json = "json";
+        Class<Object> type = Object.class;
+        given(mapper.readValue(json, type)).willReturn(o);
+        // when
+        Object result = underTest.java(json, type);
+        // then
+        assertThat(result).isEqualTo(o);
+    }
+
+    @Test
+    void itShouldNotConvertJsonToJavaAndThrow() throws Exception {
+        // given
+        String json = "json";
+        Class<Object> type = Object.class;
+        given(mapper.readValue(json, type)).willThrow(JsonProcessingException.class);
+        // when & then
+        assertThatThrownBy(() -> underTest.java(json, type))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("JACKSON CANNOT CONVERT JSON " + json + " TO JAVA OBJECT OF TYPE " + type);
+    }
+}
