1764ff9896c61b0aa4df18c5ee8e7fe80fb5d7df
xdavide9 <biancodavidetech@gmail.com>
2024-01-02 18:51:50 +0100

    Handle exceptions in UserExceptionsHandler
    handled UserNotFoundException.java and UserCannotBeModifiedException.java by returning appropriate responses to clients (also using UserExceptionReason)

--- src/main/java/com/xdavide9/sso/exception/user/api/UserExceptionsHandler.java
+++ src/main/java/com/xdavide9/sso/exception/user/api/UserExceptionsHandler.java
@@ -1,19 +1,56 @@
 package com.xdavide9.sso.exception.user.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
  * This class holds methods annotated with {@link ExceptionHandler}
  * that handle exceptions related to the User api by returning appropriate
- * http responses to clients.
+ * http responses to clients. A special enum constant {@link UserExceptionReason} is passed to
+ * these exceptions in order to further customize the responses.
  * @author xdavide9
  * @since 0.0.1-SNAPSHOT
  */
 @ControllerAdvice
 public class UserExceptionsHandler {
     @ExceptionHandler(value = UserNotFoundException.class)
-    public void handleUserNotFoundException() {
-        // TODO handle user not found exception
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException e) {
+        String error;
+        switch (e.getReason()) {
+            case INFORMATION -> error = "Cannot get information about user";
+            case DELETION -> error = "Cannot delete user";
+            case DEMOTION -> error = "Cannot demote user";
+            case PROMOTION -> error = "Cannot promote user";
+            default -> error = "User not found";
+        }
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("error", error);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("status", NOT_FOUND.toString());
+        return new ResponseEntity<>(responseBody, NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = UserCannotBeModifiedException.class)
+    public ResponseEntity<Map<String, Object>> handleUserCannotBeModifiedException(UserCannotBeModifiedException e) {
+        String error;
+        switch (e.getReason()) {
+            case INFORMATION -> error = "Cannot get information about user";
+            case DELETION -> error = "Cannot delete user";
+            case DEMOTION -> error = "Cannot demote user";
+            case PROMOTION -> error = "Cannot promote user";
+            default -> error = "User cannot be modified";
+        }
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("error", error);
+        responseBody.put("message", e.getMessage());
+        responseBody.put("status", CONFLICT.toString());
+        return new ResponseEntity<>(responseBody, CONFLICT);
     }
 }
